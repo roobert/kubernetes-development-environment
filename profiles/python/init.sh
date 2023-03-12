8ee5b9faefadff641fb678609dc47422ff5cd0ee
Rob Wilson <roobert@gmail.com>
2023-03-12 11:42:26 +0000

    Improve namespace handling

--- profiles/python/init.sh
+++ profiles/python/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Install gcp-sdk and gcsfuse
 apt-get update
 apt-get -y install lsb-release curl lsb-core apt-transport-https ca-certificates \
 	curl python-is-python3 python3-venv python3-pip
@@ -10,6 +11,12 @@ GCSFUSE_REPO="gcsfuse-${DISTRO_CODENAME}"
 
 echo "deb https://packages.cloud.google.com/apt ${GCSFUSE_REPO} main" | tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
 apt-get update
-apt-get -y install gcsfuse software-properties-common git
+apt-get -y install gcsfuse google-cloud-sdk software-properties-common git
+
+# Install Neovim
+add-apt-repository -y ppa:neovim-ppa/unstable
+apt-get update
+apt-get -y install neovim unzip zsh
